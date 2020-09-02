860c56440c0ef5ab709b7e5bb8024491621b61ac
nalbam <me@nalbam.com>
2020-09-02 18:06:31 +0900

    rm ns

--- eks-demo-charts/95-demo-dev.tf
+++ eks-demo-charts/95-demo-dev.tf
@@ -1,33 +1,33 @@
-# demo-dev
+# # demo-dev
 
-resource "kubernetes_namespace" "demo-dev" {
-  count = var.argo_cd_enabled ? 1 : 0
+# resource "kubernetes_namespace" "demo-dev" {
+#   count = var.argo_cd_enabled ? 1 : 0
 
-  metadata {
-    labels = {
-      istio-injection = "enabled"
-    }
+#   metadata {
+#     labels = {
+#       istio-injection = "enabled"
+#     }
 
-    name = "demo-dev"
-  }
-}
+#     name = "demo-dev"
+#   }
+# }
 
-# for argo-rollouts
-resource "helm_release" "demo-dev-http-benchmark" {
-  count = var.argo_cd_enabled ? 1 : 0
+# # for argo-rollouts
+# resource "helm_release" "demo-dev-http-benchmark" {
+#   count = var.argo_cd_enabled ? 1 : 0
 
-  repository = "https://kubernetes-charts-incubator.storage.googleapis.com"
-  chart      = "raw"
+#   repository = "https://kubernetes-charts-incubator.storage.googleapis.com"
+#   chart      = "raw"
 
-  namespace = "demo-dev"
-  name      = "http-benchmark"
+#   namespace = "demo-dev"
+#   name      = "http-benchmark"
 
-  values = [
-    file("./values/argo/http-benchmark.yaml")
-  ]
+#   values = [
+#     file("./values/argo/http-benchmark.yaml")
+#   ]
 
-  depends_on = [
-    kubernetes_namespace.demo-dev,
-    helm_release.argo-rollouts,
-  ]
-}
+#   depends_on = [
+#     kubernetes_namespace.demo-dev,
+#     helm_release.argo-rollouts,
+#   ]
+# }
--- eks-demo-charts/95-demo-prod.tf
+++ eks-demo-charts/95-demo-prod.tf
@@ -1,33 +1,33 @@
-# demo-prod
+# # demo-prod
 
-resource "kubernetes_namespace" "demo-prod" {
-  count = var.argo_cd_enabled ? 1 : 0
+# resource "kubernetes_namespace" "demo-prod" {
+#   count = var.argo_cd_enabled ? 1 : 0
 
-  metadata {
-    labels = {
-      istio-injection = "enabled"
-    }
+#   metadata {
+#     labels = {
+#       istio-injection = "enabled"
+#     }
 
-    name = "demo-prod"
-  }
-}
+#     name = "demo-prod"
+#   }
+# }
 
-# for argo-rollouts
-resource "helm_release" "demo-prod-http-benchmark" {
-  count = var.argo_cd_enabled ? 1 : 0
+# # for argo-rollouts
+# resource "helm_release" "demo-prod-http-benchmark" {
+#   count = var.argo_cd_enabled ? 1 : 0
 
-  repository = "https://kubernetes-charts-incubator.storage.googleapis.com"
-  chart      = "raw"
+#   repository = "https://kubernetes-charts-incubator.storage.googleapis.com"
+#   chart      = "raw"
 
-  namespace = "demo-prod"
-  name      = "http-benchmark"
+#   namespace = "demo-prod"
+#   name      = "http-benchmark"
 
-  values = [
-    file("./values/argo/http-benchmark.yaml")
-  ]
+#   values = [
+#     file("./values/argo/http-benchmark.yaml")
+#   ]
 
-  depends_on = [
-    kubernetes_namespace.demo-prod,
-    helm_release.argo-rollouts,
-  ]
-}
+#   depends_on = [
+#     kubernetes_namespace.demo-prod,
+#     helm_release.argo-rollouts,
+#   ]
+# }
