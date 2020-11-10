392ad07237352f2ad5afbbd02592716e5c505a5d
nalbam <me@nalbam.com>
2020-11-10 16:08:53 +0900

    compact list

--- db-demo/03-locals.tf
+++ db-demo/03-locals.tf
@@ -13,7 +13,7 @@ locals {
   db_username = local.db_creds.username != "" ? local.db_creds.username : var.db_username
   db_password = local.db_creds.password != "" ? local.db_creds.password : var.db_username
 
-  allow_ip_address = concat(
+  allow_ip_address = compact(concat(
     data.terraform_remote_state.vpc.outputs.public_subnet_cidr,
     data.terraform_remote_state.vpc.outputs.private_subnet_cidr,
     formatlist("%s/32", data.terraform_remote_state.vpc.outputs.nat_gateway_ips),
@@ -21,5 +21,5 @@ locals {
       "121.136.56.224/32", # echo "$(curl -sL icanhazip.com)/32"
       "61.82.141.212/32",  # echo "$(curl -sL icanhazip.com)/32"
     ],
-  )
+  ))
 }
